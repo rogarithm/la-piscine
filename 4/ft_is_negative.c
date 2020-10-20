3a934ef1bb1f9fc69e8036e1187cfebad7cbc8f3
Sehun Kim <rogarithm@gmail.com>
2020-10-20 18:10:12 +0900

    newthough: divide place to retrieve

--- 4/ft_is_negative.c
+++ 4/ft_is_negative.c
@@ -6,18 +6,21 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 20:45:22 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/19 21:01:11 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/10/20 18:07:41 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_is_negative(int n);
+void	ft_is_negative(int n)
 {
-	char str[1];
+	char positive;
+	char negative;
 
-	if ( n < 0 )
-		write(1, &n, 1);
-	else if
-		write(1, &n, 1);
+	positive = 'P';
+	negative = 'N';
+	if ( write(1, &n, 1) < 0 )
+		write(1, &negative, 1);
+	else if ( (write(1, &n, 1) >= 0 ))
+		write(1, &positive, 1);
 }
