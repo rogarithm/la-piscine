5658f53b252e801ac8e445e87064e31a71aea06e
Sehun Kim <rogarithm@gmail.com>
2020-10-21 02:12:47 +0900

    print works but not configured

--- test-for-5/ft_print_array.c
+++ test-for-5/ft_print_array.c
@@ -6,7 +6,7 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 18:42:16 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/21 00:18:29 by sh               ###   ########.fr       */
+/*   Updated: 2020/10/21 02:11:41 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,22 @@ void	ft_print_array(void)
 {
 	int	length;
 	int	counter;
-	char	arr[4];
+	char	*arr;
 
-	arr[0] = 0;
-	arr[1] = 1;
-	arr[2] = 2;
-	arr[3] = 3;
+	arr = "0123";
 	counter = 0;
 	length = 4;
 	while (counter < length)
 	{
-		if (counter < length)
+		if (counter < length - 1)
 		{
-			write(1, arr[counter], 1);
+			write(1, &arr[counter], 1);
 			counter++;
 			write(1, ", ", 2);
 		}
-		else if (counter == length)
+		else if (counter == length - 1)
 		{
-			write(1, arr[counter], 1);
+			write(1, &arr[counter], 1);
 			counter++;
 		}
 	}
