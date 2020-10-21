1a8c80f7d3ca8837fd4bc3ff83c10e0d4fe73bae
Sehun Kim <rogarithm@gmail.com>
2020-10-22 07:50:17 +0900

    ex01 check the output. unclear io treat

--- 1/ft_ultimate_ft.c
+++ 1/ft_ultimate_ft.c
@@ -6,9 +6,10 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 22:24:40 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/22 01:13:24 by sh               ###   ########.fr       */
+/*   Updated: 2020/10/22 07:44:41 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 /*
 ** to declare **ptr, what I should do?
 ** **ptr is a value pointed by *ptr.
@@ -16,10 +17,8 @@
 ** If every middle value should be declared,
 ** should I declare the pointer nine times?
 */
-void	ft_ultimate_ft(int **ptr)
-{
-	int pointer;
-	int **pointer;
 
-	**pointer = 42;
+void		ft_ultimate_ft(int *********ptr)
+{
+	*********ptr = 42;
 }
--- 1/main.c
+++ 1/main.c
@@ -6,19 +6,43 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 22:26:07 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/22 01:21:07 by sh               ###   ########.fr       */
+/*   Updated: 2020/10/22 07:47:47 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ultimate_ft(int **nbr);
+/*
+** b = &a;
+** *b = a;
+** **b = *a;
+*/
+
+#include	<stdio.h>
+
+void	ft_ultimate_ft(int *********nbr);
 
 int		main(void)
 {
-	int a;
-	int **b;
-
-	b = &a;
-	*b = a;
-	**b = *a;
-	ft_ultimate_ft(b);
+	int val;
+	int *ptr;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+	
+	val = 42;
+	ptr = &val;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+	
+	printf("%d\n", *********ptr9);
 }
