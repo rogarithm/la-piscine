8869e6926aeaf8773c1915eafda7d9a852414ad8
Sehun Kim <rogarithm@gmail.com>
2020-10-22 09:07:49 +0900

    test for ex03. additional verify required

--- 3/main.c
+++ 3/main.c
@@ -6,12 +6,26 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 22:33:03 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/21 22:33:52 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/10/22 09:05:30 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include	<stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod);
+
 int	main(void)
 {
-	ft_div_mod(3, 4, &a, &b);
+	int *div;
+	int *mod;
+	int a;
+	int b;
+
+	a = 12;
+	b = 10;
+	div = &a;
+	mod = &b;
+	ft_div_mod(12, 10, &a, &b);
+	printf("%d %d should be 1 2\n", *div, *mod);
 	return (0);
 }
