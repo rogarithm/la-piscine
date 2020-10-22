45c13f34aff1264142a406ce2a88ed00f66baa30
Sehun Kim <rogarithm@gmail.com>
2020-10-22 19:47:32 +0900

    ex04 ft complete. main require refine

--- 4/ft_ultimate_div_mod.c
+++ 4/ft_ultimate_div_mod.c
@@ -6,12 +6,21 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 22:34:56 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/21 22:37:06 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/10/22 19:43:43 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	*a = a / b;
-	*b = a % b;
+	int temp_a;
+	int temp_b;
+	int mod;
+	int remainder;
+
+	temp_a = *a;
+	temp_b = *b;
+	mod = temp_a / temp_b;
+	remainder = temp_a % temp_b;
+	*a = mod;
+	*b = remainder;
 }
