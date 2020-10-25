3eadd0350cd0dd58d4aeb5b2cfc85beca7df63df
Sehun Kim <sehukim@student.42seoul.kr>
2020-10-25 19:48:01 +0900

    ex08 complete

---
+++ 8/ft_strlowcase.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 19:32:14 by sehukim           #+#    #+#             */
+/*   Updated: 2020/10/25 19:47:33 by sehukim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int counter;
+
+	counter = 0;
+	while (str[counter])
+	{
+		if ((65 <= str[counter]) && (str[counter] <= 90))
+			str[counter] = str[counter] + 32;
+		counter++;
+	}
+	return (str);
+}
---
+++ 8/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 19:31:53 by sehukim           #+#    #+#             */
+/*   Updated: 2020/10/25 19:47:36 by sehukim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str);
+
+int	main(void)
+{
+	char Alpha[] = {"ABCDE"};
+	char alpha[] = {"abcde"};
+	
+	printf("%s should be abcde\n", ft_strlowcase(Alpha));
+	printf("%s should be abcde\n", ft_strlowcase(alpha));
+
+	return (0);
+}
