9d746376888ba8c9a52a9f4c079926b84bd24e12
Sehun Kim <sehukim@student.42seoul.kr>
2020-10-27 04:48:26 +0900

    simplified test2.c but not work. require more work

--- ex00/test.c
+++ ex00/test.c
@@ -6,7 +6,7 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 22:49:07 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/27 04:47:11 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/10/27 04:47:05 by sehukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		ft_strcmp(char *s1, char *s2)
 		if ((s1[count] > s2[count]) || (s1[count] < s2[count]))
 			return (s1[count] - s2[count]);
 		else
-				count++;
-			return (0);
+			count++;
 	}
+	return (0);
 }
