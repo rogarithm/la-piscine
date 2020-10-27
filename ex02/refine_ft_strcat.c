dc3a00f3ad6a6b4563b2302e5a7fb554fce004b8
Sehun Kim <rogarithm@gmail.com>
2020-10-27 17:11:45 +0900

    abort trap 6. do i need to start again?

--- ex02/refine_ft_strcat.c
+++ ex02/refine_ft_strcat.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   refine_ft_strcat.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 02:32:19 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/27 03:54:17 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/10/27 17:06:24 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	str_len(char *str)
+int		str_len(char *str)
 {
 	int counter;
 
@@ -21,8 +21,10 @@ int	str_len(char *str)
 	}
 	return (counter);
 }
-
-char *cat(char *dest, char *src, int dest_c)
+/* from my decision, char should be assigned including
+** when dest_len + src_len
+*/
+char	*cat(char *dest, char *src, int dest_c)
 {
 	int dest_len;
 	int src_len;
@@ -31,27 +33,34 @@ char *cat(char *dest, char *src, int dest_c)
 	src_c = 0;
 	src_len = str_len(src);
 	dest_len = str_len(dest);
-	while (dest_c < src_len + dest_len)
+	while (dest_c <= src_len + dest_len - 1)
 	{
 		dest[dest_c] = src[src_c];
 		dest_c++;
 		src_c++;
 	}
+	dest[dest_c] = '\0';
 	return (dest);
 }
 
-char *ft_strcat(char *dest, char *src)
+/*
+** as concatenate should start from null char's position
+** increase dest_count by 1 at last
+*/
+char	*ft_strcat(char *dest, char *src)
 {
 	int dest_count;
 	int dest_len;
 
 	dest_len = str_len(dest);
 	dest_count = 0;
-	if (dest[dest_count] == '\0')
+	if (dest == '\0')
 		return (src);
-	while (dest[dest_count] < dest_len)
-	{
-		dest_count++;
-	}
+	else
+		while (dest_count < dest_len)
+		{
+			dest_count++;
+		}
+	dest_count = dest_count + 1;
 	return (cat(dest, src, dest_count));
 }
