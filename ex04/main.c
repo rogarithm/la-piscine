777ece2826b33ee1a874289045242d973e980005
Sehun Kim <sehukim@student.42seoul.kr>
2020-10-29 00:00:24 +0900

    small changes for testing files

---
+++ ex04/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+   //initializing character pointer
+   char *str1 = "Don't panic!";
+   char *str2 = "pan";
+
+   //displaying both string
+   printf("str1 = %s\n\n", str1);
+   printf("str2 = %s\n\n", str2);
+
+   printf("Remaining part of str1 after the first"
+          "occurence of str2 = %s\n", strstr(str1, str2));
+
+   return 0;
+}//end main+
