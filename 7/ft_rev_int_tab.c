d74371e656a73fcd2cec692d4eecdd28168039be
Sehun Kim <rogarithm@gmail.com>
2020-10-30 20:44:28 +0900

    commit for pull

---
+++ 7/ft_rev_int_tab.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sh <marvin@42.fr>                          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 21:03:20 by sh                #+#    #+#             */
+/*   Updated: 2020/10/23 22:41:46 by sh               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** tab is a pointer, and size is a number of int inside an array
+** what the pointer points? does size should always be equal to the array's size?
+*/
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	
+}
--- 7/main.c
+++ 7/main.c
@@ -3,30 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: sh <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/21 22:35:54 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/23 22:27:31 by sh               ###   ########.fr       */
+/*   Created: 2020/10/22 21:01:32 by sh                #+#    #+#             */
+/*   Updated: 2020/10/22 21:03:13 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+void	ft_rev_int_tab(int *tab, int size);
 
-void	ft_ultimate_div_mod(int *a, int *b);
-
-int		main(void)
+int	main(void)
 {
-	int *p1;
-	int *p2;
-	int v_of_1;
-	int v_of_2;
-
-	v_of_1 = 12;
-	v_of_2 = 10;
-	p1 = &v_of_1;
-	p2 = &v_of_2;
-	printf("When we put arguments %d & %d, then ", v_of_1, v_of_2);
-	ft_ultimate_div_mod(p1, p2);
-	printf("the result should be %d & %d. second is remainder.\n", *p1, *p2);
+	ft_rev_int_tab(tab, size);
 	return (0);
 }
