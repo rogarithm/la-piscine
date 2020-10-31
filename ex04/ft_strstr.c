2c1f45888a1a358a44607ff8300a953dab474862
Sehun Kim <sehukim@student.42seoul.kr>
2020-10-31 21:32:25 +0900

    merge master for 2 worked files

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -6,10 +6,11 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 23:13:32 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/29 01:06:18 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/10/31 21:31:57 by sehukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	str_len(char *str)
+
+int		str_len(char *str)
 {
 	int counter;
 
@@ -23,30 +24,36 @@ int	str_len(char *str)
 
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int count;
-	unsigned int n_count;
+	int				count;
+	unsigned int	n_count;
 
 	count = 0;
 	n_count = 0;
-	while (((s1[count] != '\0') && (s2[count] != '\0')) && (n_count < n - 1))
+	//	while (((s1[count] != '\0') && (s2[count] != '\0')) && (n_count < n - 1))
+	while (((s1[count] != '\0') || (s2[count] != '\0')) && (n_count < n))
 	{
 		if (s1[count] != s2[count])
 			return (s1[count] - s2[count]);
 		else
+		{
 			count++;
-		n_count++;
+			n_count++;
+		}
 	}
-	return (s1[count] - s2[count]);
+	return (0); 
 }
-/* if we find the position of str that to_find matched, by using
- ** method of moving pointer itself, returning the pointer gives
- ** us the whole string except the characters unmatched before.
- **
- ** when to_find is null, as it searches nothing, the output
- ** should be null. Likewise, when src is null and to_find is
- ** null, the output should be null.
- */
-char *ft_strstr(char *str, char *to_find)
+
+/*
+** if we find the position of str that to_find matched, by using
+** method of moving pointer itself, returning the pointer gives
+** us the whole string except the characters unmatched before.
+**
+** when to_find is null, as it searches nothing, the output
+** should be null. Likewise, when src is null and to_find is
+** null, the output should be null.
+*/
+
+char	*ft_strstr(char *str, char *to_find)
 {
 	int find_len;
 
@@ -55,7 +62,7 @@ char *ft_strstr(char *str, char *to_find)
 	find_len = str_len(to_find);
 	while (*str)
 	{
-		if ((*str == *to_find) && 
+		if ((*str == *to_find) &&
 				(ft_strncmp(str, to_find, find_len) == 0))
 			return (str);
 		ft_strstr(str++, to_find);
