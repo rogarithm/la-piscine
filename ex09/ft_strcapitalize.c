16f24736f7fe6dc728534416dc91da5fe0d07c0d
Sehun Kim <sehukim@student.42seoul.kr>
2020-11-02 17:53:35 +0900

    complete ex09

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,35 +6,44 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 19:50:00 by sehukim           #+#    #+#             */
-/*   Updated: 2020/11/02 15:43:31 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/11/02 17:53:23 by sehukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** 28-33: First, convert every uppercase into lower case.
+** if of 30: if you increase cnt here (cnt++), it will just increase 1.
+** 35: move until you find the first elem of word. despite you removed
+** lower case, you need to add it on this condition to filter non-word
+** elem only.
+** 40: if it's not a number, make it upper case
+** 43: after changing, move position to next non-word elem
+*/
+
 char	*ft_strcapitalize(char *str)
 {
-	int counter;
+	int cnt;
 
-	counter = 1;
-	while (str[counter - 1])
-		if (('A' <= str[counter - 1]) && (str[counter - 1] <= 'Z')) // convert every uppercase into lower case
-		{
-			str[counter - 1] = str[counter - 1] + 32;
-			counter++;
-		}
-	while (str[counter])
+	cnt = 0;
+	while (str[cnt])
 	{
-		// move until you find the first elem of word. despite you removed
-		// lower case, you need to add it on this condition to filter just non-word elem.
-		while ((!((97 <= str[counter]) && (str[counter] <= 122))) 
-				&& (!((48 <= str[counter]) && (str[counter] <= 57)))
-				&& (!((48 <= str[counter]) && (str[counter] <= 57))))
-			counter++;
-		if (('a' <= str[counter]) && (str[counter] <= 'z')) // if it's not number, make it upper case
-			str[counter] = str[counter] - 32;
-		while ((!((97 <= str[counter]) && (str[counter] <= 122))) // after changing, move position to next non-word elem
-				&& (!((48 <= str[counter]) && (str[counter] <= 57)))
-				&& (!((48 <= str[counter]) && (str[counter] <= 57))))
-			counter++;
+		if (('A' <= str[cnt]) && (str[cnt] <= 'Z'))
+			str[cnt] = str[cnt] + 32;
+		cnt++;
+	}
+	cnt = 0;
+	while (str[cnt])
+	{
+		while ((!((97 <= str[cnt]) && (str[cnt] <= 122)))
+				&& (!((65 <= str[cnt]) && (str[cnt] <= 90)))
+				&& (!((48 <= str[cnt]) && (str[cnt] <= 57))))
+			cnt++;
+		if (('a' <= str[cnt]) && (str[cnt] <= 'z'))
+			str[cnt] = str[cnt] - 32;
+		cnt++;
+		while (((97 <= str[cnt]) && (str[cnt] <= 122))
+				|| ((48 <= str[cnt]) && (str[cnt] <= 57)))
+			cnt++;
 	}
 	return (str);
 }
--- ex09/main.c
+++ ex09/main.c
@@ -6,7 +6,7 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 19:49:26 by sehukim           #+#    #+#             */
-/*   Updated: 2020/11/02 15:39:56 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/11/02 17:53:21 by sehukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ char	*ft_strcapitalize(char *str);
 
 int	main(void)
 {
-	char Alpha[] = {"salut, comment ty vas ? 42mots quarante-deux; cinquante+et+un"};
+	char Alpha[] = "salut, comment ty vas ? 42mots quarante-deux; cinquante+et+un";
 	
-	printf("%s should be\n", ft_strcapitalize(Alpha));
+	char *str = ft_strcapitalize(Alpha);
+	printf("%s should be\n", str);
 	printf("Salut, Comment Tu Vas ? 42mots Quarante-Deux; Cinquante+Et+Un");
 	return (0);
 }
