c58b703f25b684bf91518d401a2d214c776e4d99
Sehun Kim <sehukim@student.42seoul.kr>
2020-11-02 15:40:27 +0900

    ex09 refine. output not printed

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 19:50:00 by sehukim           #+#    #+#             */
-/*   Updated: 2020/10/28 11:39:44 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/11/02 15:39:57 by sehukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,25 @@ char	*ft_strcapitalize(char *str)
 	int counter;
 
 	counter = 1;
-	while (str[counter])
+	while (*str)
+		if (('A' <= str[counter - 1]) && (str[counter - 1] <= 'Z')) // convert every uppercase into lower case
+		{
+			str[counter - 1] = str[counter - 1] + 32;
+			counter++;
+		}
+	while (*str)
 	{
-		if (('a' <= str[counter - 1]) && (str[counter - 1] <= 'z'))
-			str[counter] = str[counter] - 32;
-		if (str[counter - 1] == 32 &&
-				(('a' <= str[counter]) && (str[counter] <= 'z')))
-			str[counter] = str[counter] - 32;
-		else if (str[counter - 1] == 45 &&
-				(('a' <= str[counter]) && (str[counter] <= 'z')))
-			str[counter] = str[counter] - 32;
-		else if (str[counter - 1] == 43 &&
-				(('a' <= str[counter]) && (str[counter] <= 'z')))
+		// move until you find the first elem of word. despite you removed
+		// lower case, you need to add it on this condition to filter just non-word elem.
+		while ((!((97 <= str[counter - 1]) && (str[counter - 1] <= 122))) 
+				&& (!((48 <= str[counter - 1]) && (str[counter - 1] <= 57)))
+				&& (!((48 <= str[counter - 1]) && (str[counter - 1] <= 57))))
+			counter++;
+		if (('a' <= str[counter - 1]) && (str[counter - 1] <= 'z')) // if it's not number, make it upper case
+			str[counter - 1] = str[counter - 1] - 32;
+		while ((!((97 <= str[counter - 1]) && (str[counter - 1] <= 122))) // after changing, move position to next non-word elem
+				&& (!((48 <= str[counter - 1]) && (str[counter - 1] <= 57)))
+				&& (!((48 <= str[counter - 1]) && (str[counter - 1] <= 57))))
 			counter++;
 	}
 	return (str);
