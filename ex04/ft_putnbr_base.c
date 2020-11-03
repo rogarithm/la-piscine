96630be0cda1530f716ade5c50d34161af4e2821
Sehun Kim <rogarithm@gmail.com>
2020-11-03 17:27:14 +0900

    make test in c file. remove ft_putnbr

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 04:46:33 by sehukim           #+#    #+#             */
-/*   Updated: 2020/11/03 00:05:37 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/11/03 17:22:14 by sh               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,4 +125,56 @@ void	ft_putnbr_base(int nbr, char *base)
 		write(1, &base[passed_a[i]], 1);
 		i--;
 	}
+}
+
+#include <stdio.h>
+#include <unistd.h>
+void ft_putnbr_base(int nbr, char *base);
+int main(void)
+{
+	ft_putnbr_base(1234567890, "0123456789");
+	write(1, "\n", 1);
+	ft_putnbr_base(65536, "01");
+	write(1, "\n", 1);
+	ft_putnbr_base(100000000, "0123456789ABCDEF");
+	write(1, "\n", 1);
+	ft_putnbr_base(2147438647, "0123456");
+	write(1, "\n", 1);
+	ft_putnbr_base(-2147438648, "01234");
+	write(1, "\n", 1);
+	ft_putnbr_base(2147438647, "0123456789ABCDEFGHIJKLMNOPQ");
+	write(1, "\n", 1);
+	write(1, "\n", 1);
+
+	ft_putnbr_base(1234567890, ")!@#$%^&*(");
+	write(1, "\n", 1);
+	ft_putnbr_base(65536, "OI");
+	write(1, "\n", 1);
+	ft_putnbr_base(100000000, "\'\"\?>.<,QWERT ABC");
+	write(1, "\n", 1);
+	ft_putnbr_base(2147438647, "aQqR Tt");
+	write(1, "\n", 1);
+	ft_putnbr_base(-2147438648, "=_|{}");
+	write(1, "\n", 1);
+	ft_putnbr_base(2147438647, "ZXCS DF12345;:'\"qwertyas@#$");
+	write(1, "\n", 1);
+
+	ft_putnbr_base(2147438647, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_= ");
+	write(1, "\n", 1);
+	ft_putnbr_base(0, "!@#$");
+	write(1, "\n", 1);
+	write(1, "\nT1: ", 5);
+	ft_putnbr_base(12345, "1234563");
+	write(1, "\nT2: ", 5);
+	ft_putnbr_base(12345, "12345-64");
+	write(1, "\nT3: ", 5);
+	ft_putnbr_base(12345, "12345678+");
+	write(1, "\nT4: ", 5);
+	ft_putnbr_base(12345, "1");
+	write(1, "\nT5: ", 5);
+	ft_putnbr_base(12345, "");
+	write(1, "\nT6: ", 5);
+	ft_putnbr_base(12345, "a12356a7");
+	write(1, "\n", 1);
+	return 0;
 }
