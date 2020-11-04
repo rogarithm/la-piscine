e4b6ce1a87044aea534b6a5ae3fc696fd71584bd
Sehun Kim <sehukim@student.42seoul.kr>
2020-11-05 01:05:40 +0900

    rm unnecessary func

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: sehukim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 16:42:35 by sehukim           #+#    #+#             */
-/*   Updated: 2020/11/05 00:57:12 by sehukim          ###   ########.fr       */
+/*   Updated: 2020/11/05 01:05:00 by sehukim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,6 @@
 ** This program displays its own name.
 ** The name means the first element of argc, if there's more than one.
 */
-
-void	ft_putstr(char *str)
-{
-	while (*str)
-	{
-		write(1, str, 1);
-		str++;
-	}
-}
 
 int	main(int argc, char **argv)
 {
